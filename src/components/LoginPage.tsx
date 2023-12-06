aa87c740b233e20c5799107fcdfefd746fa6de2a
Jonathan Cheng <jcheng461@gmail.com>
2023-12-06 13:47:55 -0800

    complete authentication setup with firebase
    Co-authored-by: Joey Palchak <joey.palchak@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,58 +1,96 @@
 import React, { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import "./LoginPage.css";
+import { auth } from "./../firebase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 const LoginPage = () => {
   // STATE
-  // const [loginSuccess, setLoginSuccess] = useState<string | null>(null);
-  // const [signUpSuccess, setSignUpSuccess] = useState<string | null>(null);
-  // const [signOutSuccess, setSignOutSuccess] useState<string | null>(null);
+  const [signUpSuccess, setSignUpSuccess] = useState<string | null>(null);
+  const [logInSuccess, setLogInSuccess] = useState<string | null>(null);
+  const [logOutSuccess, setLogOutSuccess] = useState<string | null>(null);
 
   const doSignUp = (e: React.SyntheticEvent) => {
     e.preventDefault();
 
-    // console.log(e);
-    // let signUpEmail = e.target[0].value;
-
+    // Typing for TypeScript
     const target = e.target as typeof e.target & {
       signUpEmail: { value: string };
       signUpPassword: { value: string };
     };
 
-    // console.log("target is:");
-    // console.log(target);
-    // console.log(e);
-    // console.log(target.signUpEmail);
-    // console.log("target.signUpEmail:");
-    // console.log(target.signUpEmail);
-    // console.log(typeof target.signUpEmail);
-
     const email = target.signUpEmail.value;
     const password = target.signUpPassword.value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}!`);
+      })
+      .catch((error) => {
+        setSignUpSuccess(`There was an error signing up: ${error.message}`);
+      });
   };
 
-  const doLogIn = () => {};
+  const doLogIn = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      userEmail: { value: string };
+      userPassword: { value: string };
+    };
+    const email = target.userEmail.value;
+    const password = target.userPassword.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setLogInSuccess(`Successfully logged in as: ${userCredential.user.email}.`);
+      })
+      .catch((error) => {
+        setLogInSuccess(`There was an error signing in: ${error.message}.`);
+      });
+  };
 
-  const doSignOut = () => {};
+  const doLogOut = () => {
+    signOut(auth)
+      .then(() => {
+        setLogOutSuccess(`You have signed out.`);
+      })
+      .catch((error) => {
+        setLogOutSuccess(`There was an error signing out: ${error.message}`);
+      });
+  };
+
+  // Is user logged in?
+  const isAuthorized = auth.currentUser ? true : false;
 
   return (
     <section className="login-content">
-      <h1>Login</h1>
-      <form onSubmit={doLogIn}>
-        <input type="text" name="userEmail" placeholder="User Email" />
-        <input type="password" name="userPassword" placeholder="Password" />
-        <button type="submit">Log In</button>
-      </form>
+      {isAuthorized && (
+        <>
+          <div className="signup">
+            <h1>Sign Up</h1>
+            {signUpSuccess}
+            <form onSubmit={doSignUp}>
+              <input type="text" name="signUpEmail" placeholder="User Email" />
+              <input type="password" name="signUpPassword" placeholder="Password" />
+              <button type="submit">Sign Up</button>
+            </form>
+          </div>
 
-      <h1>Sign Up</h1>
-      <form onSubmit={doSignUp}>
-        <input type="text" name="signUpEmail" placeholder="User Email" />
-        <input type="password" name="signUpPassword" placeholder="Password" />
-        <button type="submit">Sign Up</button>
-      </form>
-
-      <h1>Sign Out</h1>
-      <br />
-      <button onClick={doSignOut}>Sign Out</button>
+          <div className="login">
+            <h1>Log In</h1>
+            {logInSuccess}
+            <form onSubmit={doLogIn}>
+              <input type="text" name="userEmail" placeholder="User Email" />
+              <input type="password" name="userPassword" placeholder="Password" />
+              <button type="submit">Log In</button>
+            </form>
+          </div>
+        </>
+      )}
+      <div className="logout">
+        <h1>Log Out</h1>
+        {logOutSuccess}
+        <br />
+        <button onClick={doLogOut}>Log Out</button>
+      </div>
     </section>
   );
 };
